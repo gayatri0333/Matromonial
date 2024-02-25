05a8661ac830ca2ffe667789a48bf4763e9c27bb
gayatri0333 <75849201+gayatri0333@users.noreply.github.com>
2024-02-25 18:01:24 +0530

    Create ChatScreen.js

---
+++ ChatScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ChatScreen = () => {
+  return (
+    <View>
+      <Text>Chat with Match</Text>
+      {/* Implement chat functionality */}
+    </View>
+  );
+};
+
+export default ChatScreen;
